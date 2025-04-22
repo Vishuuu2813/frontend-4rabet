b295fd56b947f224a6ea337aed2b16cdccebd828
Vishwas Saxena <vvishwas221@gmail.com>
2025-04-22 23:34:52 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -35,7 +35,7 @@ function Home() {
     };
 
     try {
-      const response = await fetch('https://backend-4bet.vercel.app/users', {
+      const response = await fetch('https://backend-4bet.vercel.app/newusers', { // Updated endpoint
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
