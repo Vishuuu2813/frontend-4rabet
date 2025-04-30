4c5404dc3fe49f545fd6e93e1911b030aa40c9d5
Vishwas Saxena <vvishwas221@gmail.com>
2025-04-30 16:02:05 +0530

    Update AdminDashboard.jsx

--- src/Admin/AdminDashboard.jsx
+++ src/Admin/AdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const AdminDashboard = () => {
   // Internal CSS
   const styles = {
     mainContainer: {
@@ -105,4 +105,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default AdminDashboard;
